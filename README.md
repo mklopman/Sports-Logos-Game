30d3b216225b0591de73757dcb5d4578f34d9323
mklopman <michaelklopman@gmail.com>
2017-06-11 22:33:06 -0400

    some styling

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Sports-Logos-Game
+
+Approach:
+
+First off, I wanted to do this how I thought a programmer would do it -- take care of the functionality first, make sure it works and then tackle the CSS. And I think that was a sound approach for me, because I could see myself going back and forth between the functionality and the css so often to the point where it'd slow me down. 
+
+I also wanted to set my game up dynamically through jQuery, while creating as few elements in my HTML file as possible. Making two HTML files crossed my mind, but I chose to set it up similar to a homework assignment we did with the racers, and change the background and hide some elements from the landing page instead. 
+
+Setup: 
+
+My goal was to have a large array of objects, each having a logo image and a corrsesponding team name. The original plan was to have an input box for the user to type in his/her guesses. But I didn't factor in all the things that could go wrong there -- the user typing in lowercase or uppercase or typos -- and decided to turn it into a multiple choice game with three wrong answers and a correct one to go with the sports team logo that was displayed. 
+
+I also wanted to randomize the array of logos, but then I realized that the array is already randomized, since I selected 20 random teams. Randomizing how the answers was more important anyways, otherwise the correct answer would be in the same position every time. 
+
+Problems:
+
+My biggest issue at the beginning with setting up my functions and figuring out which elements I should create with global scope. I thought I did a better job at pseudo coding this time around, which was a big help in structuring my code and making it clean. 
+
+Keeping it dry was a whole other problem later on when I was making progress. It took some time condensing my code into cleaner functions that worked. 
--- main.js
+++ main.js
@@ -41,146 +41,130 @@ $(document).ready(function() {
         wrong1: "Colorado Avalanche",
         wrong2: "Vancouver Canucks",
         wrong3: "Pittsburgh Penguins",
-        img: "./logos/caps.jpg",
-        weight: 5
+        img: "./logos/caps.jpg"
     }, {
         name: "Portland Trail Blazers",
         wrong1: "Washington Wizards",
         wrong2: "OkC Thunder",
         wrong3: "Sacramento Kings",
-        img: "./logos/blazers.jpg",
-        weight: 5
+        img: "./logos/blazers.jpg"
     }, {
         name: "Toronto Blue Jays",
         wrong1: "Baltimore Orioles",
         wrong2: "St. Louis Cardinals",
         wrong3: "Miami Marlins",
-        img: "./logos/bluejays.jpg",
-        weight: 5
+        img: "./logos/bluejays.jpg"
     }, {
         name: "Houston Texans",
         wrong1: "Dallas Cowboys",
         wrong2: "Indianapolis Colts",
         wrong3: "Buffalo Bills",
-        img: "./logos/texans.jpg",
-        weight: 5
+        img: "./logos/texans.jpg"
     }, {
         name: "Milwaukee Brewers",
         wrong1: "Chicago Cubs",
         wrong2: "San Diego Padres",
         wrong3: "Cincinnati Reds",
-        img: "./logos/brewers.jpg",
-        weight: 5
+        img: "./logos/brewers.jpg"
     }, {
         name: "Chicago Bulls",
         wrong1: "Los Angeles Lakers",
         wrong2: "Milwaukee Bucks",
         wrong3: "Detroit Pistons",
-        img: "./logos/bulls.jpg",
-        weight: 5
+        img: "./logos/bulls.jpg"
     }, {
         name: "Anaheim Ducks",
         wrong1: "The Mighty Ducks",
         wrong2: "Nashville Predators",
         wrong3: "Calgary Flames",
-        img: "./logos/ducks.jpg",
-        weight: 5
+        img: "./logos/ducks.jpg"
     }, {
         name: "Jacksonville Jaguars",
         wrong1: "Kansas City Chiefs",
         wrong2: "Denver Broncos",
         wrong3: "Chicago Bears",
-        img: "./logos/jags.jpg",
-        weight: 5
+        img: "./logos/jags.jpg"
     }, {
         name: "Miami Heat",
         wrong1: "Orlando Magic",
         wrong2: "Los Angeles Clippers",
         wrong3: "Cleveland Cavaliers",
-        img: "./logos/heat.jpg",
-        weight: 5
+        img: "./logos/heat.jpg"
     }, {
         name: "Minnesota Twins",
         wrong1: "Detroit Tigers",
         wrong2: "Kansas City Royals",
         wrong3: "Houston Astros",
-        img: "./logos/twins.jpg",
-        weight: 5
+        img: "./logos/twins.jpg"
     }, {
         name: "Golden State Warriors",
         wrong1: "Philadelphia 76ers",
         wrong2: "Seattle Supersonics",
         wrong3: "Houston Rockets",
-        img: "./logos/warriors.jpg",
-        weight: 5
+        img: "./logos/warriors.jpg"
     }, {
         name: "Tennessee Titans",
         wrong1: "Houston Oilers",
         wrong2: "Los Angeles Rams",
         wrong3: "Miami Dolphins",
-        img: "./logos/titans.jpg",
-        weight: 5
+        img: "./logos/titans.jpg"
     }, {
         name: "Buffalo Sabres",
         wrong1: "San Jose Sharks",
         wrong2: "Arizona Coyotes",
         wrong3: "Minnesota Wild",
-        img: "./logos/sabres.jpg",
-        weight: 5
+        img: "./logos/sabres.jpg"
     }, {
         name: "Philadelphia Phillies",
         wrong1: "Washington Nationals",
         wrong2: "Colorado Rockies",
         wrong3: "Pittsburgh Pirates",
-        img: "./logos/phillies.jpg",
-        weight: 5
+        img: "./logos/phillies.jpg"
     }, {
         name: "Indiana Pacers",
         wrong1: "Boston Celtics",
         wrong2: "Atlanta Hawks",
         wrong3: "Dallas Mavericks",
-        img: "./logos/pacers.jpg",
-        weight: 5
+        img: "./logos/pacers.jpg"
     }, {
         name: "Tampa Bay Lightning",
         wrong1: "Los Angeles Chargers",
         wrong2: "St. Louis Blues",
         wrong3: "Ottawa Senators",
-        img: "./logos/lightning.jpg",
-        weight: 5
+        img: "./logos/lightning.jpg"
     }, {
         name: "New England Patriots",
         wrong1: "Green Bay Packers",
         wrong2: "Minnesota Vikings",
         wrong3: "San Francisco 49ers",
-        img: "./logos/patriots.jpg",
-        weight: 5
+        img: "./logos/patriots.jpg"
     }, {
         name: "New York Mets",
         wrong1: "New York Yankees",
         wrong2: "Texas Rangers",
         wrong3: "Oakland Athletics",
-        img: "./logos/mets.jpg",
-        weight: 5
+        img: "./logos/mets.jpg"
     }, {
         name: "New Orleans Saints",
         wrong1: "Seattle Seahawks",
         wrong2: "New York Jets",
         wrong3: "Cleveland Browns",
-        img: "./logos/saints.jpg",
-        weight: 5
+        img: "./logos/saints.jpg"
     }, {
         name: "Detroit Red Wings",
         wrong1: "Winnipeg Jets",
         wrong2: "New Jersey Devils",
         wrong3: "Dallas Stars",
-        img: "./logos/redwings.jpg",
-        weight: 5
+        img: "./logos/redwings.jpg"
     }]
 
     var body = $('body'); // creating global variable for body
     var playButton = $('<button>');
-	var frontButton = $('#play');
+    var frontButton = $('#play');
+
+    var container = $('<div>');
+    container.attr("id", "container");
+    
 
     var countdown = $('<div>');
     countdown.attr("id", "counter");
@@ -188,18 +172,16 @@ $(document).ready(function() {
     var scoreCard = $('<div>');
     scoreCard.attr("id", "score")
 
-
-
-    var selectedLogos = []
+	var selectedLogos = []
     var rightAnswers = []
-
-   	var currentQ = 0
-
-	var randomLogo = $('<img>'); // declaring a new img in memory
+    var currentQ = 0
+    var randomLogo = $('<img>'); // declaring a new img in memory
     var right = $('<button>');
 
 
-//add attribution!!
+
+
+    //add attribution!!
     // var shuffleLogos = function(logos) {
     //     for (var i = logos.length - 1; i > 0; i--) {
     //         var j = Math.floor(Math.random() * (i + 1));
@@ -213,51 +195,6 @@ $(document).ready(function() {
     // var randomizedLogos = shuffleLogos(logos);
 
     // var getTeam = function(array){
-	 
-    var currentTeam = {};
-    var displayNextQuestion = function() {
-    	currentTeam = logos[currentQ];
-    	// console.log(currentTeam);
-
-    	var currentLogo = $('<img>').attr("src", currentTeam.img).addClass("logo");
-
-
-		var possibleAnswers = [
-			$('<button>').text(currentTeam.name).addClass("correctAnswer").on("click", correctGuess),
-		    $('<button>').text(currentTeam.wrong1).addClass("wrong1").on("click", function(){alert("Try Again!")}),
-		    $('<button>').text(currentTeam.wrong2).addClass("wrong2").on("click", function(){alert("Try Again!")}),
-		    $('<button>').text(currentTeam.wrong3).addClass("wrong3").on("click", function(){alert("Try Again!")})
-		];
-
-		var shuffleAnswers = function(possibleAnswers) {
-	        for ( var i = possibleAnswers.length - 1; i > 0; i--) {
-	            var j = Math.floor(Math.random() * (i + 1));
-				var temp = possibleAnswers[i];
-				possibleAnswers[i] = possibleAnswers[j];
-				possibleAnswers[j] = temp;
-	        }
-	        return possibleAnswers;
-    	}
-
-    	var shuffledAnswers = shuffleAnswers(possibleAnswers);
-
-    	var createQuestionDiv = function() {
-    		var questionDiv = $("<div>").addClass("question-div");
-	     	body.append(questionDiv);
-	     	questionDiv.append(currentLogo);
-		    questionDiv.append(shuffledAnswers[0]);
-		    questionDiv.append(shuffledAnswers[1]);
-		    questionDiv.append(shuffledAnswers[2]);
-		    questionDiv.append(shuffledAnswers[3]);
-    	}
-
-    	createQuestionDiv();
-
-    }
-
-
-
-
     var sec = 60;
     var timer = function() {
         setInterval(function() {
@@ -270,6 +207,57 @@ $(document).ready(function() {
     }
 
 
+
+    var currentTeam = {};
+    var displayNextQuestion = function() {
+        currentTeam = logos[currentQ];
+
+        var currentLogo = $('<img>').attr("src", currentTeam.img).addClass("logo");
+
+
+        var possibleAnswers = [
+            $('<button>').text(currentTeam.name).addClass("option correctAnswer").on("click", correctGuess),
+            $('<button>').text(currentTeam.wrong1).addClass("option wrong1").on("click", function() { alert("Try Again!") }),
+            $('<button>').text(currentTeam.wrong2).addClass("option wrong2").on("click", function() { alert("Try Again!") }),
+            $('<button>').text(currentTeam.wrong3).addClass("option wrong3").on("click", function() { alert("Try Again!") })
+        ];
+
+        var shuffleAnswers = function(possibleAnswers) {
+            for (var i = possibleAnswers.length - 1; i > 0; i--) {  //* got the shuffle Array function from stack overflow
+                var j = Math.floor(Math.random() * (i + 1));		// link: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+                var temp = possibleAnswers[i];
+                possibleAnswers[i] = possibleAnswers[j];
+                possibleAnswers[j] = temp;
+            }
+            return possibleAnswers;
+        }
+
+        var shuffledAnswers = shuffleAnswers(possibleAnswers);
+
+        var createQuestionDiv = function() {
+            var questionDiv = $("<div>").addClass("question-div");
+            container.append(questionDiv);
+            questionDiv.append(currentLogo);
+            questionDiv.append(shuffledAnswers[0]);
+            questionDiv.append(shuffledAnswers[1]);
+            questionDiv.append(shuffledAnswers[2]);
+            questionDiv.append(shuffledAnswers[3]);
+        }
+
+        createQuestionDiv();
+
+    }
+
+
+
+    function trackScore() {
+    	var score = $('<p>');
+    	score.text(currentQ);
+    	scoreCard.append(score);
+    	container.append(scoreCard);
+    }
+
+
     //this button starts the game
     playButton.click(function() {
         play();
@@ -277,14 +265,13 @@ $(document).ready(function() {
     })
 
     function game() {
-    	displayNextQuestion();
+        displayNextQuestion();
     }
 
     function checkWin() {
-    	if(rightAnswers.length === 20){
-    		alert("YOU WON!");
-    		timer.stop();
-    	}
+        if (rightAnswers.length === 20) {
+            alert("YOU WON!");
+        }
     }
 
     function play() {
@@ -295,28 +282,26 @@ $(document).ready(function() {
     }
 
     function createGame() {
-        body.css("background-color", "blue");
+        body.css("background-image", "url(https://www.nationalacademyofathletics.com/bbbg.jpg");
+        $('#gameTitle').hide();
         frontButton.hide();
         playButton.attr("id", "begin");
         playButton.text("BEGIN!");
+        container.show();
         body.append(playButton);
+        body.append(container);
     }
-    function correctGuess(){
-    	rightAnswers.push(currentTeam);
-    	console.log(rightAnswers);
-    	currentQ += 1
-    	$(".question-div").hide();
-    	checkWin();
-    	displayNextQuestion();
-    	// finalScore += logos[currentQ].weight;
-    	// console.log(finalScore);
-    	// make this a for loop and make it for the length - 1 of logos array
-    	// for(var i = 0; i < logos.length - 1; i++){
-    
-    	
-    	}
-    	
-    
+
+    function correctGuess() {
+        rightAnswers.push(currentTeam);
+        currentQ += 1
+        trackScore();
+        $(".question-div").hide();
+        checkWin();
+        displayNextQuestion();
+    }
+
+
 
     // this button creates the game page
     frontButton.click(function() {
@@ -327,35 +312,3 @@ $(document).ready(function() {
 
 
 });
-
-
-
-
-
-
-    	// var right = $('<button>').text(currentTeam.name).on("click", function() {
-	    //     	currentQ += 1
-	    //     	selectedLogos.push()
-	    //     	console.log(currentQ);
-	    //     });
-		   //  var incorrectOne = $('<button>').text(currentTeam.wrong1);
-		   //  var incorrectTwo = $('<button>').text(currentTeam.wrong2);
-		   //  var incorrectThree = $('<button>').text(currentTeam.wrong3);
-
-
-    	// for
-	    // var possibleAnswers = [
-	    // 	var right = $('<button>').text(currentTeam.name).on("click", function() {
-	    //     	currentQ += 1
-	    //     	selectedLogos.push()
-	    //     	console.log(currentQ);
-	    //     });
-		   //  var incorrectOne = $('<button>').text(currentTeam.wrong1);
-		   //  var incorrectTwo = $('<button>').text(currentTeam.wrong2);
-		   //  var incorrectThree = $('<button>').text(currentTeam.wrong3);
-	    // ]
-
-	    // var randomIndex = //random number
-	    // var randomElement = possibleAnswers[randomIndex]
-	    // body.append(randomElement)
-	    //remove that element from array
